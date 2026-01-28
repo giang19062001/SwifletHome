6e1ba65d7e385852152814b159c4bc797f15c55d
Giang ngô <meobapgiang19062001@gmail.com>
2026-01-29 00:20:09 +0700

    Fix PUBLIC_ROUTERS

--- src/modules/auth/auth.config.ts
+++ src/modules/auth/auth.config.ts
@@ -1,6 +1,6 @@
 import { CookieOptions } from 'express';
 
-export const PUBLIC_ROUTERS = ['/privacy-policy', '/support-center',, '/qrcode-global/:id']
+export const PUBLIC_ROUTERS = ['/privacy-policy', '/support-center',, '/qrcode-global']
 export const AUTH_CONFIG = {
   TOKEN_NAME: 'swf-token',
   EXPIRED_ADMIN: '1d' as const,
@@ -8,7 +8,7 @@ export const AUTH_CONFIG = {
   EXPIRED_APP_NONE_SAVE: '7d' as const,
   COOKIE_ADMIN_CONFIG: {
     httpOnly: false, //  có thể truy cập bằng JavaScript
-    sameSite: 'lax', //  chỉ gửi khi request cùng site # login redirect từ domain khác → lỗi
+    sameSite: 'lax', //  login redirect từ domain khác → có thể
     secure: true, // HTTPS
     maxAge: 24 * 60 * 60 * 1000, // 1day
   } satisfies CookieOptions,
