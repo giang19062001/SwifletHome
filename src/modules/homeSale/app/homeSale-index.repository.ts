25815828cc7a363fcf18cf349e4e89b1ae815587
Giang ngô <meobapgiang19062001@gmail.com>
2026-04-18 09:27:45 +0700

    Fix query of getHomeSale

--- src/modules/homeSale/app/homeSale-index.repository.ts
+++ src/modules/homeSale/app/homeSale-index.repository.ts
@@ -10,12 +10,12 @@ export class HomeSaleIndexAppRepository {
   constructor(@Inject('MYSQL_CONNECTION') private readonly db: Pool) {}
 
   async getTotal(): Promise<number> {
-    const [rows] = await this.db.query<RowDataPacket[]>(` SELECT COUNT(seq) AS TOTAL FROM ${this.table}`);
+    const [rows] = await this.db.query<RowDataPacket[]>(` SELECT COUNT(seq) AS TOTAL FROM ${this.table} WHERE isActive = 'Y'`);
     return rows.length ? (rows[0].TOTAL as number) : 0;
   }
   async getAll(dto: PagingDto): Promise<HomeSaleResDto[]> {
     const [rows] = await this.db.query<RowDataPacket[]>(
-      ` SELECT  A.seq, A.homeCode, A.homeName, A.homeAddress,A.homeDescription, A.latitude, A.longitude, A.homeImage, A.isActive,
+      ` SELECT  A.seq, A.homeCode, A.homeName, A.homeAddress, A.latitude, A.longitude, A.homeImage, A.isActive,
           COALESCE(
               JSON_ARRAYAGG(
                   JSON_OBJECT('seq', B.seq,'filename', B.filename,'mimetype', B.mimetype)
@@ -24,8 +24,9 @@ export class HomeSaleIndexAppRepository {
           ) AS homeImages
       FROM tbl_home_sale A
       LEFT JOIN tbl_home_sale_img B ON A.seq = B.homeSeq
+      WHERE A.isActive = 'Y'
       GROUP BY 
-          A.seq, A.homeCode, A.homeName, A.homeAddress, A.homeDescription, A.latitude, A.longitude, A.homeImage, A.isActive 
+          A.seq, A.homeCode, A.homeName, A.homeAddress, A.latitude, A.longitude, A.homeImage, A.isActive 
          ${dto.limit == 0 && dto.page == 0 ? '' : 'LIMIT ? OFFSET ?'} `,
       dto.limit == 0 && dto.page == 0 ? [] : [dto.limit, (dto.page - 1) * dto.limit],
     );
