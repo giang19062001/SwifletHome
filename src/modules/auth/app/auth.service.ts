d8469d834068e2695d4f2a30207d36ca5dbc811e
Giang ngô <meobapgiang19062001@gmail.com>
2026-05-15 14:32:16 +0700

    Test docker-compose 1

--- src/modules/auth/app/auth.service.ts
+++ src/modules/auth/app/auth.service.ts
@@ -341,7 +341,7 @@ export class AuthAppService extends AbAuthService {
       };      
       // cập nhật loại user đang hoạt động - chỉ cập nhập khi type cũ và type mới khác nhau
       if(payload.userTypeKeyWord !== isUserTypeValid.userTypeKeyWord){
-        const msgForward =  `Thay đổi ${payload.userTypeKeyWord} sang ${isUserTypeValid.userTypeKeyWord} của user(${payload.userCode})`
+        const msgForward =  `Thay đổi ${payload.userTypeKeyWord} sang ${isUserTypeValid.userTypeKeyWord} của USER(${payload.userCode})`
         this.userAppService.upsertUserTypeLive(payload.userCode, dto.userTypeCode, msgForward);
       }
       // ky lại token mới
